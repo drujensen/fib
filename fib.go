d02aac2e0f4530435699492aeec4d6b2494f96b7
Alexander F. Rødseth <rodseth@gmail.com>
2018-09-25 10:54:29 +0200

    Add fib-mem.go and fib-constexpr.cpp

--- fib.go
+++ fib.go
@@ -2,24 +2,13 @@ package main
 
 import "fmt"
 
-func fib(n uint64, cache map[uint64]uint64) uint64 {
-	if n > 1 {
-		a, ok := cache[n-1]
-		if !ok {
-			a = fib(n-1, cache)
-			cache[n-1] = a
-		}
-		b, ok := cache[n-2]
-		if !ok {
-			b = fib(n-2, cache)
-			cache[n-2] = b
-		}
-		return a + b
+func fib(n int) int {
+	if n <= 1 {
+		return 1
 	}
-	return 1
+	return fib(n-2) + fib(n-1)
 }
 
 func main() {
-	cache := make(map[uint64]uint64)
-	fmt.Println(fib(uint64(46), cache))
+	fmt.Println(fib(46))
 }
