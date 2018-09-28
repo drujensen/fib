cce3664d4051924545d1c1c00808b04bff3f677d
James Milne <shakna-israel@users.noreply.github.com>
2018-09-29 02:46:19 +1000

    fib-mem.lua, Removed else branching

--- fib-mem.lua
+++ fib-mem.lua
@@ -2,16 +2,10 @@ local nums = {}
 local fib
 
 fib = function(n)
-  if n <= 1 then
-    return 1
-  else
-    if nums[n] then
-      return nums[n]
-    else
-      nums[n] = fib(n - 1) + fib(n - 2)
-      return nums[n]
-    end
-  end
+  if n <= 1 then return 1 end
+  if nums[n] then return nums[n] end
+  nums[n] = fib(n - 1) + fib(n - 2)
+  return nums[n]
 end
 
 print(fib(46))
