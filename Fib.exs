3b7a2c1285bdd8315a3310c8f0b5aab5934fac62
Omar Bahareth <omar@omar.engineer>
2018-09-28 12:22:03 +0300

    Use one liner function style

--- Fib.exs
+++ Fib.exs
@@ -1,7 +1,7 @@
 defmodule Fib do
-  def fib(0) do 1 end
-  def fib(1) do 1 end
-  def fib(n) do fib(n-1) + fib(n-2) end
+  def fib(0), do: 1
+  def fib(1), do: 1
+  def fib(n), do: fib(n-1) + fib(n-2)
 end
 
 IO.puts Fib.fib(46)
