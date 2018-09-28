52c2a518e63f66281159a1d67a34901397947b8b
Douglas Bagnall <douglas@halo.gen.nz>
2018-09-28 23:42:48 +1200

    fib.c: use unsigned long
    This is consistent with others and performs better with GCC 6.3.

--- fib.c
+++ fib.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-long fib(long n) {
+unsigned long fib(unsigned long n) {
   if (n <= 1) return 1;
   return fib(n - 1) + fib(n - 2);
 }
 
 int main(void) {
-  printf("%li", fib(46));
+  printf("%lu", fib(46));
   return 0;
 }
