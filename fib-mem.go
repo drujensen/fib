b152a6a730e2785a1748d4cf2b675221dc6b8e92
Olivier Mengué <dolmen@cpan.org>
2018-09-28 14:26:13 +0200

    fib-mem.go: faster and shorter

--- fib-mem.go
+++ fib-mem.go
@@ -4,21 +4,19 @@ import (
 	"fmt"
 )
 
-func fib(n uint64, cache []uint64) uint64 {
-	if n <= 1 {
-		return 1
+func fib(n int, cache *[]uint64) uint64 {
+	if n >= len(*cache) {
+		a := fib(n-2, cache)
+		b := fib(n-1, cache)
+		*cache = append(*cache, a+b)
 	}
-	a, b := &cache[n-1], &cache[n-2]
-	if *a == 0 {
-		*a = fib(n-1, cache)
-	}
-	if *b == 0 {
-		*b = fib(n-2, cache)
-	}
-	return *a + *b
+	return (*cache)[n]
 }
 
 func main() {
-	cache := make([]uint64, 46)
-	fmt.Println(fib(46, cache))
+	const n = 46
+	cache := make([]uint64, 2, n)
+	cache[0] = 1
+	cache[1] = 1
+	fmt.Println(fib(n, &cache))
 }
