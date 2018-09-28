a4d9862b0257c30319029af2b0d9bba3df0ac64a
Tom Hennigan <tomhennigan@gmail.com>
2018-09-28 16:14:18 +0100

    Remove usage comments.
    This is consistent with the other examples which are documented in the readme.

--- fib.pyx
+++ fib.pyx
@@ -1,8 +1,3 @@
-# Usage:
-#   cython3 --embed -o fib.pyx.c fib.pyx
-#   g++ -O3 -o fib fib.pyx.c -I/usr/include/python3.5m/ -lpython3.5m
-#   time ./fib
-
 cdef long fib(long n):
   if n <= 1: return 1
   return fib(n - 1) + fib(n - 2)
