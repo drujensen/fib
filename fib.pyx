e1f52d153e04782cc7879050e3f92c8777427022
Tom Hennigan <tomhennigan@gmail.com>
2018-09-28 13:28:49 +0100

    Add Cython benchmark code
    Seems competitive with the other compiled/typed languages:
    
    ```
    $ cython3 --embed -o fib.c fib.pyx
    $ g++ -O3 -o fib fib.c -I/usr/include/python3.5m/ -lpython3.5m 
    $ time ./fib 
    2971215073
    
    real	0m4.361s
    user	0m4.357s
    sys	0m0.004s
    ```
    
    For reference the C version takes ~4.1s on my machine.

---
+++ fib.pyx
@@ -0,0 +1,10 @@
+# Usage:
+#   cython3 --embed -o fib.c fib.pyx
+#   g++ -O3 -o fib fib.c -I/usr/include/python3.5m/ -lpython3.5m
+#   time ./fib
+
+cdef long fib(long n):
+  if n <= 1: return 1
+  return fib(n - 1) + fib(n - 2)
+
+print(fib(46))
