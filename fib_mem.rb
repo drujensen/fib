ea348a4bff273eca1f5b78714a9fd75b701f72c5
Kevin Chiu <kevin.gc@gmail.com>
2018-09-28 12:17:12 -0400

    changed to look like existing example

--- fib_mem.rb
+++ fib_mem.rb
@@ -1,10 +1,8 @@
-@mem = [0, 1]
+@cache = [0, 1]
 def fib(n)
-    if n == 0
-        return 1
-    end
-  return @mem[n] if @mem[n]
-  @mem[n] = fib(n - 1) + fib(n - 2)
+  return 1 if n <= 1
+  return @cache[n] if @cache[n]
+  @cache[n] = fib(n - 1) + fib(n - 2)
 end
 
 puts fib(46)
