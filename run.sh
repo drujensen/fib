1c2aa714c1486a45bde7e114355cc4088a4b8b73
Dru Jensen <drujensen@gmail.com>
2018-09-28 18:03:09 -0700

    format decimal to 3f

--- run.sh
+++ run.sh
@@ -25,6 +25,9 @@ languages << Language.new("Rust", :compiled, "rustc -O fib.rs", "time ./fib")
 languages << Language.new("D", :compiled, "ldc2 -O3 -release -flto=full -of=fib fib.d", "time ./fib")
 languages << Language.new("Go", :compiled, "go build fib.go", "time ./fib")
 languages << Language.new("Swift", :compiled, "swiftc -O -g fib.swift", "time ./fib")
+languages << Language.new("OCaml", :compiled, "ocamlopt -O3 -o fib fib.ml", "time ./fib")
+languages << Language.new("Haskell", :compiled, "ghc -O3 -o fib fib.hs", "time ./fib")
+languages << Language.new("Fortran", :compiled, "gfortran -O3 -o fib fib.f03", "time ./fib")
 
 languages << Language.new("Java", :vm, "javac Fib.java", "time java Fib")
 languages << Language.new("C#", :vm, "dotnet build -c Release -o ./bin", "time dotnet ./bin/fib.dll")
@@ -35,7 +38,14 @@ languages << Language.new("Julia", :mixed, "", "time julia fib.jl")
 languages << Language.new("Node", :mixed, "", "time node fib.js")
 languages << Language.new("Elixir", :mixed, "", "time elixir fib.exs")
 
-languages << Language.new("Ruby", :interpreted, "", "time ruby fib.rb")
+#languages << Language.new("Ruby", :interpreted, "", "time ruby fib.rb")
+#languages << Language.new("Php", :interpreted, "", "time php fib.php")
+#languages << Language.new("Python", :interpreted, "", "time python fib.py")
+#languages << Language.new("Python3", :interpreted, "", "time python3 fib.py")
+#languages << Language.new("Perl", :interpreted, "", "time perl fib.pl")
+#languages << Language.new("Perl 6", :interpreted, "", "time perl6 fib.p6")
+#languages << Language.new("R", :interpreted, "", "time r -f fib.r")
+#languages << Language.new("Tcl", :interpreted, "", "time tclsh fib.tcl")
 
 languages.each do |lang|
   puts "Running #{lang.name}"
@@ -51,7 +61,7 @@ puts "|------------|-----------|----------------------------------------------|-
 languages.select {|l| l.type == :compiled}.sort_by {|l| l.run_time}.each do |lang|
     results = []
     results << lang.name.ljust(10, " ")
-    results << lang.run_time.to_s.rjust(8, " ")
+    results << ("%.3f" % lang.run_time).rjust(8, " ")
     results << lang.compile_cmd.ljust(45, " ")
     results << lang.run_cmd.ljust(12, " ")
     puts "| #{results.join(" | ")} |"
@@ -65,7 +75,7 @@ puts "|-----------|---------|------------------------------------|--------------
 languages.select {|l| l.type == :vm}.sort_by {|l| l.run_time}.each do |lang|
     results = []
     results << lang.name.ljust(9, " ")
-    results << lang.run_time.to_s.rjust(8, " ")
+    results << ("%.3f" % lang.run_time).rjust(8, " ")
     results << lang.compile_cmd.ljust(36, " ")
     results << lang.run_cmd.ljust(27, " ")
     puts "| #{results.join(" | ")} |"
@@ -79,7 +89,7 @@ puts "|-----------|---------|-----------------------------|"
 languages.select {|l| l.type == :mixed}.sort_by {|l| l.run_time}.each do |lang|
     results = []
     results << lang.name.ljust(9, " ")
-    results << lang.run_time.to_s.rjust(8, " ")
+    results << ("%.3f" % lang.run_time).rjust(8, " ")
     results << lang.run_cmd.ljust(27, " ")
     puts "| #{results.join(" | ")} |"
 end
@@ -93,7 +103,7 @@ puts "|-----------|---------|-----------------------------|"
 languages.select {|l| l.type == :interpreted}.sort_by {|l| l.run_time}.each do |lang|
     results = []
     results << lang.name.ljust(9, " ")
-    results << lang.run_time.to_s.rjust(8, " ")
+    results << ("%.3f" % lang.run_time).rjust(8, " ")
     results << lang.run_cmd.ljust(27, " ")
     puts "| #{results.join(" | ")} |"
 end
