8ce872d20bfc8073ff2104217f0cda3f62f17fa9
Joe Landman <landman@scalableinformatics.com>
2018-10-01 12:19:49 -0400

    Added inline call to C fib function.  No memoizing, but will require `sudo cpan Inline::C` be run before running this for the first time.

---
+++ fib-inline.pl
@@ -0,0 +1,13 @@
+#!/opt/nlytiq/bin/perl
+
+use Inline C => <<'...';
+ 
+long fib(long n) {
+	         return ( n > 1 ? fib(n - 2) + fib(n - 1) : 1 );
+	       } 
+...
+
+$L = shift;
+      
+print fib($L)
+      
