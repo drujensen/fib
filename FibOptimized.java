6f49097d5906b504a272bf837e435ac95fb9d10f
Issa Khoury <i_kh@icloud.com>
2018-10-03 12:41:32 +0300

    Refactored optimized java fib

--- FibOptimized.java
+++ FibOptimized.java
@@ -9,23 +9,19 @@ public class FibOptimized{
         BigInteger fibOne = new BigInteger("1");
         // It is initialized to the second fibonacci number in the series
         BigInteger fibTwo = new BigInteger("1");
-        // This is a counter used as a cursor over the fibonacci series
-        long count = 0;
         // We will loop over all numbers until we reach "i"
-        for(int s = 0; s <= i; s++){
+        for(int cursor = 0; cursor <= i; cursor++){
             // For the first two fibonacci numbers, we can return with a no ops since fibOne and fibTwo are already set with the correct answer
-            if(s == 0 || s ==1)
+            if(cursor == 0 || cursor ==1)
                 continue;
             // If the current number is even
-            else if(count % 2 == 0){
+            else if(cursor % 2 == 0){
                 fibTwo = fibTwo.add(fibOne);
             }
             // if the current number is odd
             else{
                 fibOne = fibOne.add(fibTwo);
             }
-            // Move the cursor to the next number
-            count+= 1;
         }
         return (fibOne.compareTo(fibTwo) > 0 ? fibOne : fibTwo);
     }
