2e3cf85095709744b55f467f6aa9783e5793b05d
Issa Khoury <i_kh@icloud.com>
2018-10-03 12:14:23 +0300

    Refactored optimized java fib

--- FibOptimized.java
+++ FibOptimized.java
@@ -1,33 +1,37 @@
 import java.math.*;
 import java.time.*;
 
+/**
+ * This class tries to compute a fibonacci number without using recursion
+ */
 public class FibOptimized{
-    public static BigInteger one = new BigInteger("1");
-    public static BigInteger two = new BigInteger("1");
-    static long count =0;
+    // It is initialized to the first fibonacci number in the series
+    private static BigInteger fibOne = new BigInteger("1");
+    // It is initialized to the second fibonacci number in the series
+    private static BigInteger fibTwo = new BigInteger("1");
+    // This is a counter used as a cursor over the fibonacci series
+    private static long count = 0;
 
-    static void fibonnaci(long i){
-        for(int s = 0; s < i; s++){
+    private static BigInteger fibonacci(long i){
+        // We will loop over all numbers until we reach "i"
+        for(int s = 0; s <= i; s++){
+            // For the first two fibonacci numbers, we can return with a no ops since fibOne and fibTwo are already set with the correct answer
             if(s == 0 || s ==1)
                 continue;
-            else if(count%2 == 0){
-                count+= 1;
-                two = two.add(one);}
+            // If the current number is even
+            else if(count % 2 == 0){
+                fibTwo = fibTwo.add(fibOne);
+            }
+            // if the current number is odd
             else{
-                count+=1;
-                one = one.add(two);}
+                fibOne = fibOne.add(fibTwo);
+            }
+            // Move the cursor to the next number
+            count+= 1;
         }
+        return (fibOne.compareTo(fibTwo) > 0 ? fibOne : fibTwo);
     }
     public static void main(String[] args){
-        if(args.length != 1){
-            System.err.println("Error! expected one numebr argument");
-            System.exit(1);
-        }
-        Instant start = Instant.now();
-        System.out.println("Processing...");
-        fibonnaci(Long.parseLong(args[0]));
-        long duration = Duration.between(start, Instant.now()).toMillis();
-        System.out.println("Result: " + (one.compareTo(two) > 0 ? one : two));
-        System.out.println("Done in " +  duration + "ms");
+        System.out.println(fibonacci(46));
     }
 }
