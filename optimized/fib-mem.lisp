7f7715c56b96a76ece09d842d0c8440d9efa7635
Masataro Asai <guicho2.71828@gmail.com>
2018-10-13 16:17:39 +0900

    fib-mem.lisp

---
+++ optimized/fib-mem.lisp
@@ -0,0 +1,25 @@
+
+(declaim (optimize (speed 3) (debug 0) (safety 0)))
+(declaim (ftype    (function (fixnum) fixnum) fib))
+
+(let ((cache (make-array 100 :element-type 'fixnum :initial-element -1)))
+  (declare ((simple-array fixnum (100)) cache))
+  (defun fib (n)
+    (if (<= n 1)
+        (setf (aref cache n) 1)
+        (let ((cached (aref cache n)))
+          (if (plusp cached)
+              cached
+              (setf (aref cache n)
+                    (the fixnum
+                         (+ (fib (1- n))
+                            (fib (- n 2))))))))))
+
+(defun main ()
+  (write (fib 46)))
+
+#+sbcl
+(sb-ext:save-lisp-and-die "fib" :toplevel #'main :executable t)
+
+#+ccl
+(ccl:save-application "fib" :toplevel-function #'main :prepend-kernel t)
