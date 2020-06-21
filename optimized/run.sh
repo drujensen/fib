ea02f014222c79f48d470bdceb691ecd0b443e48
Dru Jensen <drujensen@gmail.com>
2020-06-21 16:33:20 -0700

    fix optimized

--- optimized/run.sh
+++ optimized/run.sh
@@ -7,46 +7,51 @@ class Language
     @type = type
     @compile_cmd = compile_cmd
     @run_cmd = run_cmd
+    @run_time = 0
   end
 
   def run
     unless compile_cmd.empty?
       raise "compile failed" unless system("#{compile_cmd} 2>/dev/null")
     end
-    times = `{ time #{run_cmd} ; } 2>&1`.split("\n")[3].split("\t")[1].split(/[m,s]/)
+    times = `{ bash -c "time #{run_cmd}" ; } 2>&1`.split("\n")[2].split("\t")[1].split(/[m,s]/)
     @run_time = (times[0].to_i * 60) + times[1].to_f
+    `rm ./fib` if run_cmd == "./fib"
+  rescue StandardError  => ex
+    puts ex.message
+    puts ex.backtrace.inspect
   end
 end
 
 languages = []
-languages << Language.new("Python Cached", :optimized, "", "time python3 fib-cache.py")
-languages << Language.new("Lisp Compile Time", :optimized, "sbcl --load fib-compiletime.lisp", "time ./fib-compiletime")
-languages << Language.new("C++ Constant", :optimized, "g++-8 -O3 -o fib-const fib-constexpr.cpp", "time ./fib-const")
-languages << Language.new("D Memoized", :optimized, "ldc2 -O3 -release -flto=full -of=fib-mem fib-mem.d", "time ./fib-mem")
-languages << Language.new("Go Memoized", :optimized, "go build fib-mem.go", "time ./fib-mem")
-languages << Language.new("Node Memoized", :optimized, "", "time node fib-mem.js")
-languages << Language.new("K Memoized", :optimized, "", "time k fib-mem.k")
-languages << Language.new("Lua Memoized", :optimized, "", "time lua fib-mem.lua")
-languages << Language.new("Nim Memoized", :optimized, "nim cpp -d:release fib_mem.nim", "time ./fib_mem")
-languages << Language.new("Nim Constant", :optimized, "nim cpp -d:release fib_const.nim", "time ./fib_cont")
-languages << Language.new("Nim Rewrite", :optimized, "nim c -d:release fib_rewrite.nim", "time ./fib_rewrite")
-languages << Language.new("Perl6 Memoized", :optimized, "", "time perl6 fib-mem.p6")
-languages << Language.new("Perl Memoized", :optimized, "", "time perl fib-mem.pl")
-languages << Language.new("Perl Memoized 2", :optimized, "", "time perl fib-mem2.pl")
-languages << Language.new("Perl Inline", :optimized, "", "time perl fib-inline.py")
-languages << Language.new("Ruby Memoized", :optimized, "", "time ruby fib-mem.rb")
-languages << Language.new("Swift Memoized", :optimized, "swiftc -O -g fib-mem.swift", "time ./fib-mem")
-languages << Language.new("Tcl Memoized", :optimized, "", "time tclsh fib-mem.tcl")
-languages << Language.new("Erlang Memoized", :optimized, "erlc +native +'{hipe,[o3]}' fib_mem.erl", "time erl -noinput -noshell -s fib_mem")
-languages << Language.new("Escript Memoized", :optimized, "", "time escript fib_mem.es")
-languages << Language.new("Haskell Memoized", :optimized, "ghc -O3 -o fib_mem fib_mem.hs", "time ./fib_mem")
-languages << Language.new("Janet Memoized", :optimized, "", "time janet ./fib-mem.janet")
-languages << Language.new("Janet TCO", :optimized, "", "time janet ./fib-tco.janet")
-languages << Language.new("OCaml TCO", :optimized, "ocamlopt -O3 -o fib_tail fib_tail.ml", "time ./fib_tail")
-languages << Language.new("Elixir Iterative", :optimized, "", "time elixir fib-iterative.exs")
-languages << Language.new("Java Iterative", :optimized, "javac FibOptimized.java", "time java FibOptimized")
-#languages << Language.new("Haskell TCO", :optimized, "ghc -O3 -o fib_tail fib_tail.hs", "time ./fib_tail")
-#languages << Language.new("Rust Memoized", :optimized, "rustc -O fib_mem.rs", "time ./fib_mem")
+languages << Language.new("Python Cached", :optimized, "", "python3 fib-cache.py")
+languages << Language.new("Lisp Compile Time", :optimized, "sbcl --load fib-compiletime.lisp", "./fib-compiletime")
+languages << Language.new("C++ Constant", :optimized, "g++-8 -O3 -o fib-const fib-constexpr.cpp", "./fib-const")
+languages << Language.new("D Memoized", :optimized, "ldc2 -O3 -release -flto=full -of=fib-mem fib-mem.d", "./fib-mem")
+languages << Language.new("Go Memoized", :optimized, "go build fib-mem.go", "./fib-mem")
+languages << Language.new("Node Memoized", :optimized, "", "node fib-mem.js")
+languages << Language.new("K Memoized", :optimized, "", "k fib-mem.k")
+languages << Language.new("Lua Memoized", :optimized, "", "lua fib-mem.lua")
+languages << Language.new("Nim Memoized", :optimized, "nim cpp -d:release fib_mem.nim", "./fib_mem")
+languages << Language.new("Nim Constant", :optimized, "nim cpp -d:release fib_const.nim", "./fib_cont")
+languages << Language.new("Nim Rewrite", :optimized, "nim c -d:release fib_rewrite.nim", "./fib_rewrite")
+languages << Language.new("Perl6 Memoized", :optimized, "", "perl6 fib-mem.p6")
+languages << Language.new("Perl Memoized", :optimized, "", "perl fib-mem.pl")
+languages << Language.new("Perl Memoized 2", :optimized, "", "perl fib-mem2.pl")
+languages << Language.new("Perl Inline", :optimized, "", "perl fib-inline.py")
+languages << Language.new("Ruby Memoized", :optimized, "", "ruby fib-mem.rb")
+languages << Language.new("Swift Memoized", :optimized, "swiftc -O -g fib-mem.swift", "./fib-mem")
+languages << Language.new("Tcl Memoized", :optimized, "", "tclsh fib-mem.tcl")
+languages << Language.new("Erlang Memoized", :optimized, "erlc +native +'{hipe,[o3]}' fib_mem.erl", "erl -noinput -noshell -s fib_mem")
+languages << Language.new("Escript Memoized", :optimized, "", "escript fib_mem.es")
+languages << Language.new("Haskell Memoized", :optimized, "ghc -O3 -o fib_mem fib_mem.hs", "./fib_mem")
+languages << Language.new("Janet Memoized", :optimized, "", "janet ./fib-mem.janet")
+languages << Language.new("Janet TCO", :optimized, "", "janet ./fib-tco.janet")
+languages << Language.new("OCaml TCO", :optimized, "ocamlopt -O3 -o fib_tail fib_tail.ml", "./fib_tail")
+languages << Language.new("Elixir Iterative", :optimized, "", "elixir fib-iterative.exs")
+languages << Language.new("Java Iterative", :optimized, "javac FibOptimized.java", "java FibOptimized")
+#languages << Language.new("Haskell TCO", :optimized, "ghc -O3 -o fib_tail fib_tail.hs", "./fib_tail")
+#languages << Language.new("Rust Memoized", :optimized, "rustc -O fib_mem.rs", "./fib_mem")
 
 languages.each do |lang|
   puts "Running #{lang.name}"
